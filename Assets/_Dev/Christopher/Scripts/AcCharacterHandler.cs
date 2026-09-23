f21d3fafe177e41e14a25a050503edd017927156
Christopher Barrera <c.barrera@ensi.lan>
2026-09-23 10:58:09 +0200

    feat: Linked attack animation with gameplay
    Prepared trigger fonction for Death and Hurt animation in AcCharacterHandler

--- Assets/_Dev/Christopher/Scripts/AcCharacterHandler.cs
+++ Assets/_Dev/Christopher/Scripts/AcCharacterHandler.cs
@@ -1,13 +1,44 @@
+using SynthOfRage.Scripts.Player;
 using UnityEngine;
 
 namespace _Dev.Christopher.Scripts
 {
+    [RequireComponent(typeof(CharacterController))]
+    [RequireComponent(typeof(PlayerObserver))]
     public class AcCharacterHandler : MonoBehaviour
     {
+        
         private static readonly int Move = Animator.StringToHash("Move");
+        private static readonly int Attack = Animator.StringToHash("Attack");
+        private static readonly int Hurt = Animator.StringToHash("Hurt");
+        private static readonly int Death = Animator.StringToHash("Death");
+
+        [Header("References")]
+        [SerializeField] private PlayerObserver playerObserver;
+        
         public Animator MyAnimator;
+        
         private CharacterController _characterController;
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        
+        private void OnEnable()
+        {
+            if (MyAnimator == null)
+            {
+                Debug.LogError(
+                    $"[{nameof(MyAnimator)}] PlayerObserver reference is missing.",
+                    this
+                );
+
+                return;
+            }
+            if (playerObserver == null)
+            {
+                playerObserver = transform.GetComponent<PlayerObserver>();
+            }
+
+            playerObserver.OnPlayerAtkL += HandleAtkLAnimation;
+        }
+        
         void Start()
         {
             _characterController = transform.GetComponent<CharacterController>();
@@ -18,5 +49,21 @@ namespace _Dev.Christopher.Scripts
         {
             MyAnimator.SetBool(Move,_characterController.velocity != Vector3.zero);
         }
+
+        private void HandleAtkLAnimation()
+        {
+            MyAnimator.SetTrigger(Attack);
+        }
+
+        private void HandleHurtAnimation()
+        {
+            MyAnimator.SetTrigger(Hurt);
+        }
+
+        private void HandleDeathAnimation()
+        {
+            MyAnimator.SetTrigger(Death);
+        }
+        
     }
 }
