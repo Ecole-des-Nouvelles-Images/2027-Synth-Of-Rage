0c835da2f9c13ae7f36ba75fd479fa480d1a6989
Christopher Barrera <c.barrera@ensi.lan>
2026-09-23 15:18:00 +0200

    feat: Added script for end attack trigger + rotation hurtbox

---
+++ Assets/_Dev/Christopher/Scripts/EndAttackTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace _Dev.Christopher.Scripts
+{
+    public class EndAttackTrigger : MonoBehaviour
+    {
+        public void AttackFinished()
+        {
+            Debug.Log("Attack Finished");
+        }
+    }
+}
--- Assets/_Dev/Christopher/Scripts/HurtboxEvent.cs
+++ Assets/_Dev/Christopher/Scripts/HurtboxEvent.cs
@@ -3,8 +3,35 @@ using UnityEngine;
 
 namespace _Dev.Christopher.Scripts
 {
+    [RequireComponent(typeof(BoxCollider))]
     public class HurtboxEvent : MonoBehaviour
     {
+        private SpriteRenderer _myParentSpriteRenderer;
+        private bool _flipX = false;
+
+        private void Awake()
+        {
+            if(_myParentSpriteRenderer == null && transform.parent != null)
+                _myParentSpriteRenderer = transform.parent.GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            if (_myParentSpriteRenderer)
+            {
+                if(_myParentSpriteRenderer.flipX && _flipX == false)
+                {
+                    transform.Rotate(0,180,0);
+                    _flipX = true;
+                }
+                if (_myParentSpriteRenderer.flipX == false && _flipX)
+                {
+                    transform.Rotate(0,-180,0);
+                    _flipX = false;
+                }
+            }
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Enemy"))
@@ -12,5 +39,7 @@ namespace _Dev.Christopher.Scripts
                 Debug.Log("Hurtbox Entered "+other.name);
             }
         }
+
+       
     }
 }
